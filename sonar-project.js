d459ecef7d7afb0c5332cc281992b8cbada25435
SusanOnuorah <69466968+SusanOnuorah@users.noreply.github.com>
2023-09-27 21:05:51 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://15.223.56.192:9000/',
+  serverUrl: 'http://35.183.23.83:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': '6b4d0d374bc582a0b2cf8b1eccf63a5d59d01398',
+	    'sonar.login': '440b9344b4051b94093e105723662a210c42c5ff',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
