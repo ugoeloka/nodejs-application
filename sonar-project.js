3588e947de154a69b6ce3ba60792a206fa523137
ugoeloka <142540892+ugoeloka@users.noreply.github.com>
2023-09-28 20:21:12 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://35.183.23.83:9000/',
+  serverUrl: 'http://172.31.21.83:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': '440b9344b4051b94093e105723662a210c42c5ff',
+	    'sonar.login': '86042ec76fc5fc6da3976e682341136d5467f3fd',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
